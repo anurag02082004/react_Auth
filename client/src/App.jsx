16ab885e1d19a558f7e14cd8e066e6975cbb100c
Anurag <anuragkumarsinha1111@gmail.com>
2025-12-14 10:12:47 +0530

    Create header  component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  return <BrowserRouter>
+  Rout
+  
+  </BrowserRouter>
 }
